7e3d9eea2acb286d4de1c4a161b22a30b128fb32
liunina <i19850511@gmail.com>
2021-12-31 22:46:20 +0800

    fix

---
+++ file-templates/templates/HelloWorld/TheComposableArchitecture.xctemplate/Default/___FILEBASENAME___Reducer.swift
@@ -0,0 +1,11 @@
+import ComposableArchitecture
+
+enum ___VARIABLE_MODULENAME___Module {}
+
+extension ___VARIABLE_MODULENAME___Module {
+    static var reducer: Reducer<___VARIABLE_MODULENAME___State, ___VARIABLE_MODULENAME___Action, ___VARIABLE_MODULENAME___Environment> {
+        .init { state, action, environment in
+            return .none
+        }
+    }
+}
---
+++ file-templates/templates/HelloWorld/TheComposableArchitecture.xctemplate/Default/___FILEBASENAME___View.Action+feature.swift
@@ -0,0 +1,8 @@
+extension ___VARIABLE_MODULENAME___View.Action {
+    var feature: ___VARIABLE_MODULENAME___Action {
+        switch self {
+        case .onAppear:
+            return .onAppear
+        }
+    }
+}
