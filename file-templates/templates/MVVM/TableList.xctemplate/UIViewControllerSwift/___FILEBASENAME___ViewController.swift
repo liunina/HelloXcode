7a15f067b943c566cd0e17780f913ff8abc4f0f2
nick.land <nick.rango.land@gmail.com>
2023-08-10 17:27:25 +0800

    优化模板

--- file-templates/templates/MVVM/TableList.xctemplate/UIViewControllerSwift/___FILEBASENAME___ViewController.swift
+++ file-templates/templates/MVVM/TableList.xctemplate/UIViewControllerSwift/___FILEBASENAME___ViewController.swift
@@ -27,7 +27,13 @@ final class ___VARIABLE_productName:identifier___ViewController: UIViewControlle
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    static func HelloWorld() -> ___VARIABLE_productName:identifier___ViewController {
+        let viewModel = ___VARIABLE_productName:identifier___ViewModel()
+        let vc = ___VARIABLE_productName:identifier___ViewController(viewModel: viewModel)
+        return vc
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mainView.reloadData()
